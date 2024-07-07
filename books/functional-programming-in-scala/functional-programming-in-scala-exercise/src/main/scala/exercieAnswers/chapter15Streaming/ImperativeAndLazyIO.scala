4e144cd2cc0eaa90be1332508cd3fac8e043714b
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-07-07 11:20:02 +1000

    Streaming: ImperativeIO + SimplePull Model

---
+++ books/functional-programming-in-scala/functional-programming-in-scala-exercise/src/main/scala/exercieAnswers/chapter15Streaming/ImperativeAndLazyIO.scala
@@ -0,0 +1,58 @@
+﻿package exercieAnswers.chapter15Streaming
+
+import exercieAnswers.chapter13IO.FreeTest.Console.IO
+
+import java.io._
+
+object ImperativeIO {
+
+  /** Imperative implementation which is low level and not really compositional */
+  def linesGt40k(filename: String): IO[Boolean] = IO {
+    val src = io.Source.fromFile(filename)
+    try {
+      var count = 0
+      val lines: Iterator[String] = src.getLines()
+      while (count <= 40000 && lines.hasNext) {
+        lines.next()
+        count += 1
+      }
+      count > 40000
+    } finally  {
+      src.close()
+    }
+  }
+
+
+  /** Lazy IO alternative */
+  def linesGt40kViaLazyIO(filename: String): IO[Int] = {
+    def lines(filename: String): IO[LazyList[String]] = IO {
+      val src = io.Source.fromFile(filename)
+      src.getLines().to(LazyList).lazyAppendedAll {
+        src.close()
+        LazyList.empty
+      }
+    }
+
+
+    /** We can now perform different function on the lazyList string without having to worry about IO stuff */
+    lines(filename).flatMap(
+      l => IO {
+        l.filter(_.trim.nonEmpty).take(40000).map(_.head).indexOfSlice("abracadabra".toList)
+      }
+    )
+
+    /**
+     * This is called Lazy IO and it can be problematic:
+     * - not resource safe: resource will only be closed if we examine and reach the end of the stream
+     * - Nothing stop us from traversing the same lazyList multiple time which will cause either:
+     *  + excessive memory usage as all element is cached in memory
+     *  + if result is not cached then we get error from reading closed file
+     * - forcing elements of stream has I/O side effect which will cause unpredictable result when two
+     *  threads traversing the LazyList at the same time
+     * - in more realistic scenarios, the LazyList[String] usage is unpredictable. It might be used in a data structure
+     *  which will store it for long period of time before it is examined. using this piece of data is not as easy any
+     *  more as it require inside knowledge. This make it bad for composition where we shouldn't need knowledge about
+     *  anything other than the type
+     * */
+  }
+}
---
+++ books/functional-programming-in-scala/functional-programming-in-scala-exercise/src/main/scala/exercieAnswers/chapter15Streaming/SimplePull.scala
@@ -0,0 +1,62 @@
+﻿package exercieAnswers.chapter15Streaming
+
+import exercieAnswers.chapter13IO.FreeTest.Console.IO
+
+import scala.annotation.tailrec
+
+
+trait Pull[+O, +R] {
+  import Pull._
+
+  /** Operates on the final result value of a pull, not each output value */
+  def flatMap[O2 >: O, R2](f: R => Pull[O2, R2]): Pull[O2, R2] = FlatMap(this, f)
+
+  /** Steps the pull until a final result is produced, accumulating an output value */
+  @tailrec
+  final def fold[A](init: A)(f: (A, O) => A): (R, A) = step(this) match {
+    case Left(r)         => (r, init)
+    case Right((hd, tl)) => tl.fold(f(init, hd))(f)
+  }
+
+  /** fold the pull, collecting all output element and discard result R */
+  def toList: List[O] = fold(List.newBuilder[O])((acc, o) => acc += o)._2.result()
+
+  def >>[O2 >: O, R2](next: => Pull[O2, R2]): Pull[O2, R2] = flatMap(_ => next)
+
+  def map[R2](f: R => R2): Pull[O, R2] = flatMap(r => Result(f(r)))
+}
+case class Result[+R](result: R) extends Pull[Nothing, R]
+case class Output[+O](value: O) extends Pull[O, Unit]
+case class FlatMap[X, +O, +R](source: Pull[O, X], f: X => Pull[O, R]) extends Pull[O, R]
+
+object Pull {
+  /** interpret pull while rewriting left nested flatmap call */
+  def step[O, R](pull: Pull[O, R]): Either[R, (O, Pull[O, R])] = pull match {
+    case Result(r) => Left(r)
+    case Output(o: O) => Right(o, Pull.done)
+    case FlatMap(source, f) => source match {
+      case FlatMap(s2, g) => step(s2.flatMap(x => g(x).flatMap(f)))
+      case other => step(other) match {
+        case Left(r) => step(f(r))
+        case Right((hd, tl)) => Right((hd, tl.flatMap(f)))
+      }
+    }
+  }
+
+  def done: Pull[Nothing, Unit] = Result(())
+
+  def fromList[O](os: List[O]): Pull[O, Unit] = os match {
+    case Nil => done
+    case h::t => Output(h) >> fromList(t)
+  }
+
+  def fromLazyList[O](os: LazyList[O]): Pull[O, Unit] = os match {
+    case LazyList() => done
+    case h #:: t => Output(h) >> fromLazyList(t)
+  }
+}
+
+object PullExamnple {
+  val p = Output(1) >> Output(2) // FlatMap(Output(1), _ => Output(2))
+  val q = p.toList // List(1,2)
+}
