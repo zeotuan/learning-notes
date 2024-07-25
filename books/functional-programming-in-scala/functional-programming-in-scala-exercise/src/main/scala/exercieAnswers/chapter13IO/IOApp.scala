153d621cf8c61315b166dbe0a86c6ffeaccb1e22
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-07-25 19:09:07 +1000

    Add utility for exposed IO type

--- books/functional-programming-in-scala/functional-programming-in-scala-exercise/src/main/scala/exercieAnswers/chapter13IO/IOApp.scala
+++ books/functional-programming-in-scala/functional-programming-in-scala-exercise/src/main/scala/exercieAnswers/chapter13IO/IOApp.scala
@@ -1,6 +1,9 @@
 ﻿package exercieAnswers.chapter13IO
 
 import exercieAnswers.chapter07Parallelism.NonBlockingPar.Par
+import exercieAnswers.chapter11Monad.Monad
+import exercieAnswers.chapter11Monad.Monad.parMonad
+import exercieAnswers.chapter13IO.Free._
 import exercieAnswers.chapter13IO.FreeTest.Console.ParMonad
 import exercieAnswers.chapter13IO.IOApp.IO
 
@@ -20,4 +23,27 @@ trait IOApp {
 
 object IOApp {
   type IO[A] = FreeTest.Console.IO[A]
+
+  def IO[A](a: => A): IO[A] = FreeTest.Console.IO[A](a)
+
+  implicit val ioMonad = freeMonad[Par]
+
+  def now[A](a: A): IO[A] = Return(a)
+
+  def fork[A](a: => IO[A]): IO[A] = par(Par.lazyUnit(())) flatMap (_ => a)
+
+  def forkUnit[A](a: => A): IO[A] = fork(now(a))
+
+  def delay[A](a: => A): IO[A] = now(()) flatMap (_ => now(a))
+
+  def par[A](a: Par[A]): IO[A] = exercieAnswers.chapter13IO.Suspend(a)
+
+  def async[A](cb: ((A => Unit) => Unit)): IO[A] =
+    fork(par(Par.async(cb)))
+
+  def Return[A](a: A): Return[Par, A] = exercieAnswers.chapter13IO.Return[Par, A](a)
+
+  import java.util.concurrent.ExecutorService
+  def unsafePerformIO[A](io: IO[A])(implicit E: ExecutorService): A =
+    Par.run(E) { io.run(FreeTest.Console.parMonad) }
 }
