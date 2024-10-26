34ac4c2b67a2d2b6da7bfa611c53f40321e25278
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-10-26 18:04:08 +1100

    Semigroupal apply to different type

--- books/Scala With Cats/cats-sandbox/src/main/scala/sandbox/C6_SemiGroupal_Applicative/SemiGroupal.scala
+++ books/Scala With Cats/cats-sandbox/src/main/scala/sandbox/C6_SemiGroupal_Applicative/SemiGroupal.scala
@@ -1,5 +1,6 @@
 ﻿package sandbox.C6_SemiGroupal_Applicative
 
+import cats.Semigroup
 import cats.implicits.catsSyntaxSemigroup
 
 /**
@@ -57,7 +58,7 @@ object SemiGroupal {
   (Option("abcd"), Option(123), Option("abc")).tupled // Some(("abcd", 123, "abc))
 
   final case class Cat(name: String, born: Int, color: String)
-  val cat1 = (Option("abcd"), Option(123), Option("abc")).mapN(Cat.apply) // Some(Cat("Garfield", 1978, "Orange & black"))
+  val cat1: Option[Cat] = (Option("abcd"), Option(123), Option("abc")).mapN(Cat.apply) // Some(Cat("Garfield", 1978, "Orange & black"))
   // Internally mapN uses Semigroupal to combine the Option values and then applies the function to the result
 
   import cats.Monoid
@@ -88,4 +89,67 @@ object SemiGroupal {
   garfield |+| heathcliff
 
   // res14: Cat2 = Cat2("GarfieldHeathcliff", 3966, List("Lasagne", "Junk Food"))
+
+  /** ========================== SemiGroupal Apply to different type ========================== */
+  object applyFuture {
+    import cats.instances.future._
+    import scala.concurrent._
+    import scala.concurrent.duration._
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val futurePair = Semigroupal[Future].product(Future("Hello"), Future(123))
+
+    // Future execute eagerly even before we call Await.result
+    Await.result(futurePair, 1.second) // res15: (String, Int) = ("Hello", 123)
+
+    val futureCats: Future[Cat2] = (
+      Future("Garfield"),
+      Future(1978),
+      Future(List("Lasagne"))
+    ).mapN(Cat2.apply)
+
+    Await.result(futureCats, 1.second) // res16: Cat2 = Cat2("Garfield", 1978, List("Lasagne"))
+  }
+
+  // Combining list using SemiGroupal produce a cartesian product instead of zipping
+  // This is similar to product in List Monad
+  object applyList {
+    import cats.instances.list._
+    Semigroupal[List].product(List(1, 2), List(3, 4)) // List[(Int, Int)] = List((1, 3), (1, 4), (2, 3), (2, 4))
+  }
+
+  // Similar to flatMap, SemiGroupal also fail fast when one of the context "falsy"
+  // even when it's possible to inspect the other context
+  object applyEither {
+    import cats.instances.either._
+    type ErrorOr[A] = Either[Seq[String], A]
+    Semigroupal[ErrorOr].product(
+      Left(Seq("Error 1")),
+      Left(Seq("Error 2"))
+    ) // ErrorOr[(Nothing, Nothing)] Left(List("Error 1"))
+  }
+
+  /**
+   * The reason the behaviour for List and Either is supprising is because they are both Monad
+   * This can also be seen in the implementation of Semigroupal for other monad like Option
+   * Since monad instance for these type are readily available, the default product implementation
+   * is provided. remember Monad is a Semigroupal but not vice versa
+   *
+   * The Future trick is possible since Future is created before composing them using Semigroupal
+   *
+   * The real parrallel happen when we provide an instance of Semigroupal (and Applicative) for a type but is  not a Monad
+   *
+   *
+   *  */
+   object productSample {
+
+    import cats.Monad
+    import cats.syntax.functor._ // for map
+    import cats.syntax.flatMap._ // for flatmap
+
+    // This explain cartesian product behaviour of Semigroupal for List
+    def product[F[_] : Monad, A, B](fa: F[A], fb: F[B]): F[(A, B)] = fa.flatMap(a => fb.map(b => (a, b)))
+  }
+
 }
+
