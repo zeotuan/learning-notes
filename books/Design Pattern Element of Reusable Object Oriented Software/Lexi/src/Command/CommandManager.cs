a2c158e33829d7a0c06dfa7c1a4a4be097ceb537
Tuan Pham <zeotuan@gmail.com>
2025-03-23 22:39:49 +1100

    add CommandManager Singleton

---
+++ books/Design Pattern Element of Reusable Object Oriented Software/Lexi/src/Command/CommandManager.cs
@@ -0,0 +1,43 @@
+﻿namespace Lexi.Command
+{
+    internal class CommandManager
+    {
+        private static readonly Lazy<CommandManager> instance = new(() => new CommandManager());
+
+        private readonly Stack<ICommand> undoStack = new();
+        private readonly Stack<ICommand> redoStack = new();
+
+        private CommandManager() 
+        {
+        }  // Private constructor to enforce singleton
+
+        public static CommandManager Instance => instance.Value;
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            undoStack.Push(command);
+            redoStack.Clear();  // Clear redo history after a new command
+        }
+
+        public void Undo()
+        {
+            if (undoStack.Count > 0)
+            {
+                ICommand command = undoStack.Pop();
+                command.UnExecute();
+                redoStack.Push(command);
+            }
+        }
+
+        public void Redo()
+        {
+            if (redoStack.Count > 0)
+            {
+                ICommand command = redoStack.Pop();
+                command.Execute();
+                undoStack.Push(command);
+            }
+        }
+    }
+}
